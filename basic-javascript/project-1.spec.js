0cea2f99512a0be33d6a102ff5f60778400a8bb8
Natalia Beckstead <toosmiravilla@gmail.com>
2020-04-22 15:57:54 -0600

    set up project, first test

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,12 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('projeckt-1.js', () => {
+    describe('helpers.multiplyByTen()', function() {
+        it('should multiply by ten', function() {
+            expect(helpers.multiplyByTen(4)).toBe(40);
+            expect(helpers.multiplyByTen(0)).toBe(0);
+            expect(helpers.multiplyByTen(-5)).toBe(-50);
+        })
+    })
+});
